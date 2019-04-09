77496825cd2376ca54b143764ca4e5d57d0550f1
Javier Telio <jtelio118@gmail.com>
2019-04-09 00:26:20 -0500

    add new models

--- django/mailings/models/mail.py
+++ django/mailings/models/mail.py
@@ -1,18 +1,44 @@
+from django.db.models import Q
 from django.utils import timezone
-from django.template import Template, Context
+from django.core.exceptions import ObjectDoesNotExist
+from django.template import Template, Context, engines
 from django.utils.translation import gettext_lazy as _
+
+from mailings.models import Template as T
 
 class Mail():
 
     """docstring for Mail"""
-    def __init__(self, template_id_or_name):
+    def __init__(self, template_id_or_name, params= {}):
         self.template_id_or_name = template_id_or_name
+        self.context = params
+
         self.django_engine = engines['django']
+
+    def send(self):
+
         self.__render()
 
+    def __get_template(self):
+
+        try:
+            self.template = T.objects.filter(Q(pk=self.template_id_or_name) | Q(name=self.template_id_or_name)).first()
+        except ObjectDoesNotExist:
+            print("there was a error to get template")
+
     def __render(self):
-        self.template = self.django_engine.from_string("Hello {{ name }}!")
-        print(self.template)
+
+        self.__get_template()
+
+        html = "{}{}{}".format(
+            self.template.brand.header_content,
+            self.template.content,
+            self.template.brand.footer_content)
+
+        self.template = self.django_engine.from_string(html)
+        print(self.template.render(self.context))
+
+
 
 
 
