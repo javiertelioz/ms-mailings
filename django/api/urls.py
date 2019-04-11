8ce6d385b45d4d0c6bc076886b31caf51774e2fe
Javier Telio <javiertelio@siclo.com>
2019-04-11 11:56:08 -0500

    remove document module

--- django/api/urls.py
+++ django/api/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from rest_framework_swagger.views import get_swagger_view
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import BrandViewSet, TemplateViewSet
